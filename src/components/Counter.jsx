fc714ab5f29086efd369dd05d5600cfea9663ac5
Amitprajapati1 <amp99188@gmail.com>
2026-09-23 08:15:29 +0530

    useEffect hook

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { use, useEffect, useState } from "react";
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
 
-   const [count, setCount] = useState(0);
-   const increment = () => {
-    setCount(count + 1)
-   };
+  useEffect(() => {
+    console.log("useEffect Hook");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("useEffect Hook");
+  }, [count1]);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const increment1 = () => {
+    setCount1(count1 + 1);
+  };
 
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={increment}>Click</button>
+      <hr />
+      <h1>{count1}</h1>
+      <button onClick={increment1}>Click</button>
     </div>
   );
 }
-
